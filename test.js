0d5a9022d15b6f67f21745b18ad7c2fe2a9b6ffb
ME <50546204+BadMachine@users.noreply.github.com>
2019-05-18 19:16:47 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,34 +2,38 @@ var btrf = require ('./scr/btrfapi');
 var bitrefill = new btrf();
 var number,operatorSlug,email,paymentMethod,token,ifProxy;
 
-number = "+79253139585";
-operatorSlug = "megafon-center-russia";
+number = "79675714702";
+operatorSlug = "beeline-russia";
 email = "1@1.ru";
-paymentMethod = "ethirium";
-token = ""; //Public API Key
+paymentMethod = "ethereum";
+token = "Token"; //Public API Key
 ifProxy = 'http://127.0.0.1:8121';
+var value = "10";
 
-bitrefill.placeOrder(number,operatorSlug,email,paymentMethod,token,ifProxy);
+ bitrefill.placeOrder(number,operatorSlug,email,value, paymentMethod,token,ifProxy);
 
+Promise.all([bitrefill.placeOrder(number,operatorSlug,email,value, paymentMethod,token,ifProxy)]).then(ok=> {
+    console.log(ok)
+})
 //Also u can use Promise.all([bitrefill.placeOrder(number,operatorSlug,email,paymentMethod,token,ifProxy)]).then(ok=>{
 //     console.log(ok)
 // })
 // To get response
 
 
-var orderID = "5cdc3e24172c3400047cad6c";
-
-bitrefill.getOrder(orderID, token);
-Promise.all([bitrefill.getOrder(orderID, token)]).then(ok=>{
-     console.log(ok)
-  })
+ var orderID = "5cdf2ea66a04b300047b3ac7";
+//
+// bitrefill.getOrder(orderID, token);
+//  Promise.all([bitrefill.getOrder(orderID, token)]).then(ok=>{
+//       console.log(ok)
+//    })
 
 //You can also use Promise.all([bitrefill.getOrder(orderID, token)]).then(ok=>{
 //      console.log(ok)
 //  })
 // To get response
 
-bitrefill.lookupNumber(number, token);
+// bitrefill.lookupNumber(number, token);
 
 //You can also use Promise.all([bitrefill.lookNumber(number)]).then(ok=>{
 //      console.log(ok)
